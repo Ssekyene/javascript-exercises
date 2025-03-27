fc6ad40940a12a92d1a314b0eaae3c7da08ca4ab
xrob101 <robertssekyene05@gmail.com>
2025-03-27 16:13:33 +0300

    pass reverString func tests cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const arr = string.split('');
+  const revArr = [];
+  let j = 0;
+  for (let i = arr.length-1; i >= 0 ; i--) {
+    revArr[j++] = arr[i];
+  }
+  const revString = revArr.join('');
+  return revString;
 };
 
 // Do not edit below this line
