9eb28184d779c51f89b6c2d3f8e16bc3b5bd7c5b
xrob101 <robertssekyene05@gmail.com>
2025-03-29 14:36:04 +0300

    implement tempConversion code to pass test cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFah) {
+  let tempInCel = (tempInFah - 32) * (5/9);
+  // rounded to one decimal place
+  tempInCel = Math.round(tempInCel * 10) / 10;
+  return tempInCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCel) {
+  let tempInFah = (tempInCel * (9/5)) + 32;
+  // rounded to one decimal place
+  tempInFah = Math.round(tempInFah * 10) / 10;
+  return tempInFah;
 };
 
 // Do not edit below this line
