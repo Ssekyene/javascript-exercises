f900cca627a85c72f9c6929762bbe463735eb53b
xrob101 <robertssekyene05@gmail.com>
2025-03-29 12:34:03 +0300

    add code to pass sumAll func test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, n) {
+  if (!Number.isInteger(a) || !Number.isInteger(n)) return 'ERROR';
+  if (a < 0 || n < 0) return 'ERROR';
+  let sum = a;
+  if (n > a) {
+    for (i = a+1; i <= n; i++) {
+      sum += i;
+    }
+  } else {
+    for (i = a-1; i >= n; i--) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
