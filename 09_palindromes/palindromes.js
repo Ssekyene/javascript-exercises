235de4b651aeac18c7c3f21e26eaf46dac7a1862
xrob101 <robertssekyene05@gmail.com>
2025-05-28 20:40:19 +0300

    pass palindrome tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const palindromeValidChars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+  const loweredCharArr = str.toLowerCase().split('');
+  // filter for a new lowercase array containing only valid characters and turn it into a string
+  const cleanedStr = loweredCharArr.filter(char => palindromeValidChars.includes(char)).join('');
 
+  // convert the cleaned string into a an array which is reversed and them convert back to string
+  const reversedCleanedStr = cleanedStr.split('').reverse().join('');
+
+  return (reversedCleanedStr === cleanedStr);
 };
 
 // Do not edit below this line
