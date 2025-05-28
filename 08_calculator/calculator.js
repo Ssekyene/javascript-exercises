aca5a1727bdb7f3880c23874f3d9bb8a9cff9725
xrob101 <robertssekyene05@gmail.com>
2025-05-28 19:50:12 +0300

    pass calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a=0, b=0) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a=0, b=0) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let prdt = 0;
+  if (arr.length) {
+    prdt = 1;
+    for (let i = 0; i < arr.length; i++) {
+      prdt *= arr[i];
+  }
+
+  }
+  return prdt;
 
 };
 
-const power = function() {
-	
+const power = function(a=0, b=0) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n <= 1) return 1;
+  else return n * factorial(n-1);
+
 };
 
 // Do not edit below this line
