3a78c098a6dc46a79806962a2aa5e3917d9fd758
xrob101 <robertssekyene05@gmail.com>
2025-05-30 16:54:29 +0300

    pass findTheOldest tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const oldestPerson = people.reduce(getOldest);
+  return oldestPerson;
 };
+
+function getOldest(oldestPerson, currentPerson) {
+  const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+  const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+  
+  return currentAge > oldestAge ? currentPerson : oldestPerson;
+}
+
+function getAge(yearOfBirth, yearOfDeath) {
+  if (!yearOfDeath) {
+    yearOfDeath = new Date().getFullYear();
+  }
+  return yearOfDeath - yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
