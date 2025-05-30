485af3879c19dcda2c120ac877092b9859c23bfe
xrob101 <robertssekyene05@gmail.com>
2025-05-30 13:10:55 +0300

    pass fibonacci tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  // apply base cases
+  if (n < 0) return 'OOPS';
+  else if (n == 0) return 0;
+  else if (n == 1) return 1;
+  else if(n == 2) return 1;
+  else return fibonacci(n-1) + fibonacci(n-2);
 
 };
 
